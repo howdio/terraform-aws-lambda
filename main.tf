b62ffda4f5a0a962d39f4a16aa0d3d8ea923b7dd
Steve Wolfe <stevenwolfe@gmail.com>
2019-10-10 09:44:08 -0400

    addressed TODOs

--- main.tf
+++ main.tf
@@ -1,12 +1,5 @@
 module "package" {
-  # TF-UPGRADE-TODO: In Terraform v0.11 and earlier, it was possible to
-  # reference a relative module source without a preceding ./, but it is no
-  # longer supported in Terraform v0.12.
-  #
-  # If the below module source is indeed a relative local path, add ./ to the
-  # start of the source string. If that is not the case, then leave it as-is
-  # and remove this TODO comment.
-  source = "modules/package"
+  source = "./modules/package"
 
   name          = var.name
   path          = var.package_path
@@ -14,14 +7,7 @@ module "package" {
 }
 
 module "function" {
-  # TF-UPGRADE-TODO: In Terraform v0.11 and earlier, it was possible to
-  # reference a relative module source without a preceding ./, but it is no
-  # longer supported in Terraform v0.12.
-  #
-  # If the below module source is indeed a relative local path, add ./ to the
-  # start of the source string. If that is not the case, then leave it as-is
-  # and remove this TODO comment.
-  source = "modules/function"
+  source = "./modules/function"
 
   name        = var.name
   description = var.description
